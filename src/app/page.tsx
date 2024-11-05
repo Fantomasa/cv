4cc4888781543354b7ea00426dd35705c2952bc7
Fantomasa <ivailoinfo@gmail.com>
2024-11-05 18:17:24 +0200

    edit nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,6 @@ import Nav from "@/components/Nav";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 
-// export type NavItem = { name: string; link: string; icon: ReactNode };
-// const navItems: Array<NavItem> = [
-//   {
-//     name: "Home",
-//     link: "/",
-//     icon: <FaHome />
-//   }
-// ];
-
 export default function Home() {
   return (
     <main className="relative flex flex-col bg-black-100 justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
