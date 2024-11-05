6dacb9d615baf66fe6101de8f0599d2150dc3d0c
Fantomasa <ivailoinfo@gmail.com>
2024-11-05 14:47:58 +0200

    edit container

--- src/components/ui/PinContainer/PinContainer.tsx
+++ src/components/ui/PinContainer/PinContainer.tsx
@@ -1,17 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import React from "react";
 import { PinContainer } from "./3d-pin";
+import { FaLocationArrow } from "react-icons/fa";
 
-export function AnimatedPin() {
+export function AnimatedPin({
+  title,
+  href,
+  description,
+  img,
+  iconLists
+}: {
+  title: string;
+  href: string;
+  description: string;
+  img: string;
+  iconLists: Array<string>;
+}) {
   return (
-    <div className="h-[40rem] w-full flex items-center justify-center ">
-      <PinContainer title="/ui.aceternity.com" href="https://twitter.com/mannupaaji">
-        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">Aceternity UI</h3>
-          <div className="text-base !m-0 !p-0 font-normal">
-            <span className="text-slate-500 ">Customizable Tailwind CSS and Framer Motion Components.</span>
+    <div className="w-full flex items-center justify-center ">
+      <PinContainer title={title} href={href}>
+        <div className="flex items-center justify-end">
+          <FaLocationArrow />
+        </div>
+        <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+          <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-black-100">
+            <img src="/bg.png" alt="bg-img" />
           </div>
-          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+          <img src={img} alt={title} className="z-10 absolute bottom-0" />
+        </div>
+        <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">{title}</h1>
+        <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">{description}</p>
+        <div className="flex items-center justify-between mt-7 mb-3">
+          <div className="flex items-center gap-2">
+            {iconLists.map((icon) => (
+              <div key={icon}>
+                <img src={icon} alt={icon} />
+              </div>
+            ))}
+          </div>
         </div>
       </PinContainer>
     </div>
