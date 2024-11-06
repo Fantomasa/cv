90764b6310f0f6731c24fd855d3b429e55ebd4a6
Fantomasa <ivailoinfo@gmail.com>
2024-11-06 16:30:52 +0200

    fix template layout

--- src/app/projects/[id]/template.tsx
+++ src/app/projects/[id]/template.tsx
@@ -5,13 +5,13 @@ import React from "react";
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <section className="absolute w-full h-[100vh] overflow-hidden">
+      <section className="abolute top-0 w-full overflow-hidden">
         <Spotlight className="-top40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
         <Spotlight className="top-10 left-full h-2/3 w-1/2" fill="#E5ACB6" />
         <Spotlight className="top-28 left-80 h-2/3 w-1/2" fill="blue" />
         <GridBackground />
+        <div className="flex w-full justify-center overflow-hidden mt-24">{children}</div>
       </section>
-      <div className="flex w-full justify-center overflow-hidden mt-24">{children}</div>
     </>
   );
 }
